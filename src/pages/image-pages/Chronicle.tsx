cabd4488b3371979c66b6df4d4fcdc51574b5aa0
sydlester <sydneylester31@gmail.com>
2024-09-14 19:29:33 -0400

    adding chron photos

--- src/pages/image-pages/Chronicle.tsx
+++ src/pages/image-pages/Chronicle.tsx
@@ -1,18 +1,52 @@
 import { DescriptionBox } from "../../components/DescriptionBox";
 import { CaptionedImage } from "../../components/CaptionedImage";
 import styles from "./imagePage.module.css";
-import { ICON_LIST } from "../../const";
 
-const testData = [
+const chronData = [
   {
-    imgUrl: ICON_LIST.chronicle2,
-    alt: "chronicle2",
-    caption: "Test Caption",
+    imgUrl: "images/chronicle/20221231_MBBvsFSU_10.png",
+    alt: "chronicle3",
+    caption: "Jon Scheyer at Duke vs. FSU",
   },
   {
-    imgUrl: ICON_LIST.wxdu2,
-    alt: "wxdu2",
-    caption: "Test Caption",
+    imgUrl: "images/chronicle/20230223_WBBvsNCSU_2.png",
+    alt: "Duke WBB",
+    caption: "Reigan Richardson vs. NCSU",
+  },
+  {
+    imgUrl: "images/chronicle/20230228_MBBvsNCState_1.png",
+    alt: "Jeremy Roach",
+    caption: "Jeremy Roach vs. NCSU",
+  },
+  {
+    imgUrl: "images/chronicle/20230228_MBBvsNCState_11.png",
+    alt: "Tyrese Proctor",
+    caption: "Tyrese Proctor & Cameron Crazies",
+  },
+  {
+    imgUrl: "images/chronicle/20240207_MBBvsND_14.png",
+    alt: "Mark Mitchell",
+    caption: "Mark Mitchell vs. Notre Dame",
+  },
+  {
+    imgUrl: "images/chronicle/20240212_MBBvsWF_1.png",
+    alt: "Flip",
+    caption: "Kyle Filipowski vs. Wake Forest",
+  },
+  {
+    imgUrl: "images/chronicle/20240212_MBBvsWF_10.png",
+    alt: "Jeremy Roach",
+    caption: "Jeremy Roach vs. Wake Forest",
+  },
+  {
+    imgUrl: "images/chronicle/20240212_MBBvsWF_14.png",
+    alt: "McCain",
+    caption: "Jared McCain vs. Wake Forest",
+  },
+  {
+    imgUrl: "images/chronicle/20240212_MBBvsWF.png",
+    alt: "McCain",
+    caption: "Jared McCain vs. Wake Forest",
   },
 ];
 
@@ -21,16 +55,13 @@ export default function Chronicle() {
     <div className={styles.wrapper}>
       <DescriptionBox title="CHRONICLE" description="Chronicle description" />
       <div className={styles.images}>
-        <CaptionedImage
-          imgUrl={testData[0].imgUrl}
-          alt={testData[0].alt}
-          caption={testData[0].caption}
-        />
-        <CaptionedImage
-          imgUrl={testData[1].imgUrl}
-          alt={testData[1].alt}
-          caption={testData[1].caption}
-        />
+        {chronData.map((img) => (
+          <CaptionedImage
+            imgUrl={img.imgUrl}
+            alt={img.alt}
+            caption={img.caption}
+          />
+        ))}
       </div>
     </div>
   );
